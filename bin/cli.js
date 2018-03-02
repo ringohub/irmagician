37f9ff02a79503d3a1af4281a41583ad7297e067
Yoshiki Aoki <ringohub@gmail.com>
2018-03-02 21:25:05 +0900

    Update commands

--- bin/cli.js
+++ bin/cli.js
@@ -30,13 +30,7 @@ Usage: ${cmd} <command> [file]
 
 Command
     list       List serial ports
-    capture    Capture a IR data
-    play       Send a IR data
-    dump       Dump written data
-    write      Write a IR data from json
-    temp       Get a Temperature data
-    info       Show irMagician Information
-    showPorts  Show device ports
+    version    Display IRMagichian's version
 
 Options
 ${helpOptions}
@@ -47,54 +41,21 @@ Examples
     ${cmd} write data.json
 `, flags);
 
-
 (async () => {
   const irm = await new IRMagician().catch(e => {
     console.error(e.message)
   });
 
-  console.log(cli.input);
-
   switch (cli.input[0]) {
+    case 'info':
+      await irm.info();
+      break;
     case 'list':
       await irm.list();
       break;
     case 'version':
       await irm.version();
       break;
-    // case 'version':
-    //   irm.version();
-    //   break;
-    // case 'capture':
-    //   irMagician.capture(cli.flags.p);
-    //   break;
-    // case 'play':
-    //   if (cli.input[1]) {
-    //     irMagician.play(cli.input[1], cli.flags.p);
-    //   } else {
-    //     irMagician.play(undefined, cli.flags.p);
-    //   }
-    //   break;
-    // case 'dump':
-    //   if (cli.input[1]) {
-    //     irMagician.dump(cli.input[1]);
-    //   } else {
-    //     irMagician.dump(undefined, cli.flags.p);
-    //   }
-    //   break;
-    // case 'write':
-    //   if (cli.input[1]) {
-    //     irMagician.write(cli.input[1], cli.flags.p);
-    //   } else {
-    //     cli.showHelp();
-    //   }
-    //   break;
-    // case 'temp':
-    //   irMagician.temp(cli.flags.p);
-    //   break;
-    // case 'info':
-    //   irMagician.info(cli.flags.p);
-    //   break;
     default:
       cli.showHelp();
       break;
