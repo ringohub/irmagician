ed72f46b8ca432377811145333ab8d77562bfb2b
Yoshiki Aoki <ringohub@gmail.com>
2018-03-02 19:33:40 +0900

    Add version

--- bin/cli.js
+++ bin/cli.js
@@ -1,65 +1,104 @@
 #! /usr/bin/env node
+const meow = require('meow');
+const IRMagician = require('../lib/irMagician');
 
-const irMagician = require("../lib/irMagician")
-const meow = require("meow")
+const flags = {
+  port: {
+    type: 'boolean',
+    alias: 'p',
+    description: 'List Ports'
+  },
+  version: {
+    type: 'boolean',
+    alias: 'v',
+    description: 'Display version'
+  }
+};
+
+const commands = {};
+
+const cmd = process.argv[1].split('/').reverse()[0];
+const helpOptions = Object.keys(flags).map(f => {
+  // Generate option documents. Example...
+  //     -v, --version  Display version
+  //     -h, --help     Display help
+  return `    -${flags[f].alias}, --${f}\t${flags[f].description}`;
+}).join('\n');
+
 const cli = meow(`
-Usage: irMagician <command> [file] 
+Usage: ${cmd} <command> [file] 
 
 Command
+    list       List serial ports
     capture    Capture a IR data
     play       Send a IR data
     dump       Dump written data
     write      Write a IR data from json
     temp       Get a Temperature data
-    info       Show irMagician Infomation
+    info       Show irMagician Information
     showPorts  Show device ports
 
 Options
-    -p, --port    Device port
+${helpOptions}
 
 Examples
-    irMagician capture
-    irMagician dump data.json
-    irMagician write data.json
-    
-`, {alias: {p: 'port'}})
+    ${cmd} capture
+    ${cmd} dump data.json
+    ${cmd} write data.json
+`, flags);
 
-switch (cli.input[0]) {
-case "capture":
-    irMagician.capture(cli.flags["p"])
-    break
-case "play":
-    if (cli.input[1]) {
-        irMagician.play(cli.input[1], cli.flags["p"])
-    } else {
-        irMagician.play(undefined, cli.flags["p"])
-    }
-    break
-case "dump":
-    if (cli.input[1]) {
-        irMagician.dump(cli.input[1])
-    } else {
-        irMagician.dump(undefined, cli.flags["p"])
-    }
-    break
-case "write":
-    if (cli.input[1]) {
-        irMagician.write(cli.input[1], cli.flags["p"])
-    } else {
-        cli.showHelp()
-    }
-    break
-case "temp":
-    irMagician.temp(cli.flags["p"])
-    break
-case "info":
-    irMagician.info(cli.flags["p"])
-    break
-case "showPorts":
-    require("../node_modules/serialport/bin/serialport-list.js")
 
-    break
-default:
-    cli.showHelp()
-    break
-}
+(async () => {
+  const irm = await new IRMagician().catch(e => {
+    console.error(e.message)
+  });
+
+  console.log(cli.input);
+
+  switch (cli.input[0]) {
+    case 'list':
+      await irm.list();
+      break;
+    case 'version':
+      await irm.version();
+      break;
+    // case 'version':
+    //   irm.version();
+    //   break;
+    // case 'capture':
+    //   irMagician.capture(cli.flags.p);
+    //   break;
+    // case 'play':
+    //   if (cli.input[1]) {
+    //     irMagician.play(cli.input[1], cli.flags.p);
+    //   } else {
+    //     irMagician.play(undefined, cli.flags.p);
+    //   }
+    //   break;
+    // case 'dump':
+    //   if (cli.input[1]) {
+    //     irMagician.dump(cli.input[1]);
+    //   } else {
+    //     irMagician.dump(undefined, cli.flags.p);
+    //   }
+    //   break;
+    // case 'write':
+    //   if (cli.input[1]) {
+    //     irMagician.write(cli.input[1], cli.flags.p);
+    //   } else {
+    //     cli.showHelp();
+    //   }
+    //   break;
+    // case 'temp':
+    //   irMagician.temp(cli.flags.p);
+    //   break;
+    // case 'info':
+    //   irMagician.info(cli.flags.p);
+    //   break;
+    default:
+      cli.showHelp();
+      break;
+  }
+
+  irm.close();
+})();
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require("./lib/irMagician")
+const IRMagician = require('./lib/irMagician');
+
+(async () => {
+  const irm = await new IRMagician().catch(e => {console.error(e.message)});
+  console.log(irm.port.path);
+  irm.version();
+  irm.close();
+})();
+
